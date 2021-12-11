a9fedfce6447a6a3cabcca702ef84f59ec4e75e1
isaacwach <isaacwach29@gmail.com>
2021-12-11 12:34:24 +0300

    added a function to get day of the week

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ var month = document.getElementById("month");
 var day = document.getElementById("day");
 
 document.getElementById("btn").addEventListener("click", function () {
-
+    var month=document.getElementById("month").value;
+    const inputtedDate = new Date(year.value, month, day.value)
+    let dayOfBirth = inputtedDate.getDay()
 });
